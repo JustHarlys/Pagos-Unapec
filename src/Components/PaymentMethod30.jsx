a4d737493fb78684e6a02b2b31ce7b8da135d454
JustHarlys <harlysmc@gmail.com>
2025-05-20 21:12:33 -0400

    Calculos y formateos de cada costo realizado

--- src/Components/PaymentMethod30.jsx
+++ src/Components/PaymentMethod30.jsx
@@ -15,36 +15,62 @@ function PaymentMethod30() {
   const {noDiscount} = useContext(GradeAndPeriodContext);
   const frontPayment = noDiscount * thirtyPercent
   const diferredCredits = noDiscount * seventyPercent
-  const admCharges = diferredCredits * 0.10
-  const diferredPayments = diferredCredits / 3
+  const admCharges = (diferredCredits * 0.10)
+  const diferredPayments = (diferredCredits / 3)
+  const diferredPaymentsFixed = (diferredCredits / 3).toFixed(2)
+
+  const admChargesPerMonth = (admCharges / 3).toFixed(2)
+  const totalFirstPayment = (frontPayment + (referenciasMayo.labTec * 3) + referenciasMayo.recursosTec + referenciasMayo.carnet).toFixed(2)
+  const monthlyTotal = ((admCharges / 3) + diferredPayments).toFixed(2)
+  const fullTermTotal = ((frontPayment + (referenciasMayo.labTec * 3) + referenciasMayo.recursosTec + referenciasMayo.carnet) + (((admCharges / 3) + diferredPayments) * 3))
+  let nf = new Intl.NumberFormat('en-US')
 
   return (
     <table className="paymentMethod30">
         <thead>
+        <h5>Pago Inmediato</h5>
           <tr>
-
+            <th>Anticipo 30%:</th>
+            <td>RD$ {frontPayment.toLocaleString()}</td>
           </tr>
           <tr>
-            <th>Anticipo 30%</th>
-            <td>{frontPayment}</td>
+            <th>Lab. Tecnología:</th>
+            <td>RD$ {(referenciasMayo.labTec * 3).toLocaleString()}</td>
           </tr>
           <tr>
-            <th>Lab. Tecnología</th>
-            <td>{referenciasMayo.labTec * 3}</td>
+            <th>Rec. Tecnológicos:</th>
+            <td>RD$ {(referenciasMayo.recursosTec).toLocaleString()}</td>
           </tr>
           <tr>
-            <th>Rec. Tecnológicos</th>
-            <td>{referenciasMayo.recursosTec}</td>
+            <th>Serv. Carnet:</th>
+            <td>RD$ {referenciasMayo.carnet}</td>
           </tr>
           <tr>
-            <th>Serv. Carnet</th>
-            <td>{referenciasMayo.carnet}</td>
-          </tr>
-          <tr>
-            <th>Total Neto</th>
-            <td>{frontPayment + (referenciasMayo.labTec * 3) + referenciasMayo.recursosTec + referenciasMayo.carnet}</td>
+            <th>Total Neto: </th>
+            <td>RD$ {nf.format(totalFirstPayment)}</td>
           </tr>
         </thead> 
+
+        <thead>
+          <h5 style={{marginTop: 30}}>Pago en 3 cuotas mensuales</h5>
+          <tr>
+            <th>Cargos Admvo: </th>
+            <td>RD$ {nf.format(admChargesPerMonth)}</td>
+          </tr>
+          <tr>
+            <th>Diferido Colegiatura: </th>
+            <td>RD$ {nf.format(diferredPaymentsFixed)}</td>
+          </tr>
+          <tr>
+            <th>Total Neto Mensual:</th>
+            <td>RD$ {nf.format(monthlyTotal)}</td>
+          </tr>
+
+          <tr>
+            <th>Total Cuatrimestre: </th>
+            <td>RD$ {nf.format(fullTermTotal)}</td>
+          </tr>
+        </thead>
     </table>
   )
 }
